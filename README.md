bd23a7415e52043226fd2d3c33f7994c4ff3fc3b
J.A. NachE <nache.nache@gmail.com>
2014-01-07 12:42:12 +0100

    optional script for build toolchain with uclibc

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 PiNAS
 =====
 
-NAS Linux Distribution Based on Debian Stable for Raspberry Pi.
+NAS Linux Distribution builded from scratch for Raspberry Pi.
 
 
 Building Distro
 ===============
-Only supported from Debian distro. Necesary packages before build: binfmt-support qemu qemu-user-static squashfs-tools
+This Distro is based on a build system, so we dont have repositories with the source code. Instead we have a build systen which download source, configure and install. Also this build system make images to be installed into SD card ready to boot.
+
+Necesary packages before build (debian package names): binfmt-support qemu qemu-user-static squashfs-tools
---
+++ opt_scripts/toolchain_uclibc.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+##############################################################################
+#
+#    PiNAS Linux Distribution builder
+#    Copyright (C) 2013 Juan Antonio Nache <ja@nache.net>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+############################################################################
+
+set -e
+
+ORIG=$PWD
+
+cd resources/
+if [ -d $PWD/buildroot ];then
+	echo "[I] Updating uclibc buildroot src..."
+	cd $PWD/buildroot
+	git pull
+	cd - >/dev/null
+else
+	echo "[I] Cloning uclibc buildroot src..."
+	git clone git://git.buildroot.net/buildroot
+fi
+cd buildroot
+
+NUMCORES=$(cat /proc/cpuinfo | grep vendor_id | wc -l)
+
+unset CC
+unset CXX
+unset LD
+unset NM
+unset AR
+unset RANLIB
+unset ARCH
+unset CROSS_COMPILE
+unset QEMU_LD_PREFIX
+
+echo "[I] Cleaning buildroot..."
+make -j $NUMCORES clean || echo "Nothing to clean"
+
+echo "[I] Configuring..."
+cp $ORIG/config/buildroot.conf ./.config
+
+make -j $NUMCORES oldconfig
+
+echo "[I] Building..."
+make -j $NUMCORES 
+
+
+
+
+
+
+
+
